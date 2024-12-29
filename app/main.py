63870e8480342c917815278912beaf891fc07c3c
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-12-28 21:26:18 -0500

    Added integer decoding function

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 import json
 import sys
 
-# import bencodepy - available if you need it!
+import bencodepy
 # import requests - available if you need it!
 
 # Examples:
 #
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
-def decode_bencode(bencoded_value):
-    if chr(bencoded_value[0]).isdigit():
-        first_colon_index = bencoded_value.find(b":")
-        if first_colon_index == -1:
-            raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
-    else:
-        raise NotImplementedError("Only strings are supported at the moment")
+# def decode_bencode(bencoded_value):
+#     if chr(bencoded_value[0]).isdigit():
+#         first_colon_index = bencoded_value.find(b":")
+#         if first_colon_index == -1:
+#             raise ValueError("Invalid encoded value")
+#         return bencoded_value[first_colon_index+1:]
+#     else:
+#         raise NotImplementedError("Only strings are supported at the moment")
 
 
 def main():
@@ -26,21 +26,27 @@ def main():
 
     if command == "decode":
         bencoded_value = sys.argv[2].encode('utf-8')
+        result = bencodepy.decode(bencoded_value)
+        if isinstance(result, bytes):
+            print(f'"{result.decode("utf-8")}"') 
+        elif isinstance(result, int):
+            print(result)
+ 
 
-        # json.dumps() can't handle bytes, but bencoded "strings" need to be
-        # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
-        def bytes_to_str(data):
-            if isinstance(data, bytes):
-                return data.decode()
+    #     # json.dumps() can't handle bytes, but bencoded "strings" need to be
+    #     # bytestrings since they might contain non utf-8 characters.
+    #     #
+    #     # Let's convert them to strings for printing to the console.
+    #     def bytes_to_str(data):
+    #         if isinstance(data, bytes):
+    #             return data.decode()
 
-            raise TypeError(f"Type not serializable: {type(data)}")
+    #         raise TypeError(f"Type not serializable: {type(data)}")
 
-        # Uncomment this block to pass the first stage
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
-    else:
-        raise NotImplementedError(f"Unknown command {command}")
+    #     # Uncomment this block to pass the first stage
+    #     print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+    # else:
+    #     raise NotImplementedError(f"Unknown command {command}")
 
 
 if __name__ == "__main__":
--- app/test.py
+++
@@ -1,6 +0,0 @@
-import bencodepy
-
-value = "i2111068885e".encode('utf-8')
-
-result = bencodepy.decode(value)
-print(result)
