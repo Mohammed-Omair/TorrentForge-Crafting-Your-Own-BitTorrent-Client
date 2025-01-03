2695e761f1fb9edba9d0d1f86e4228f938652231
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2025-01-02 22:21:14 -0500

    Added  function to get peers

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import sys
 import bencodepy
 import hashlib
+import requests
 def decode_bytes(obj):
     """
     Recursively decode all bytes objects in a nested structure.
@@ -25,7 +26,17 @@ def info(file):
     decoded_result = decode_bytes(result)  # Recursively decode bytes objects
     info_dict = bencodepy.encode(result[b'info'])
     info_dict = hashlib.sha1(info_dict).hexdigest()
-    print(f"Tracker URL: {decoded_result['announce']} Length: {decoded_result['info']['length']} Info Hash: {info_dict} Piece Length: {decoded_result['info']['piece length']} Piece Hashed: {decoded_result['info']['pieces']}")  # Pretty-print the result as JSON
+    return result, decoded_result
+
+def peers(result, decoded_result):
+    info_dict_byte = bencodepy.encode(result[b'info'])
+    info_dict_byte = hashlib.sha1(info_dict_byte).digest()
+    payload = {'info_hash': info_dict_byte, 'peer_id': "imomairutamasterscse", 'port': 6881, 'uploaded': 0, 'downloaded': 0, 'left': decoded_result['info']['length'], 'compact': 1}
+    r = requests.get(decoded_result['announce'], params=payload)
+    result = bencodepy.decode(r.content)  # Decode the bencoded value
+    decoded_result = decode_bytes(result)
+    print(decoded_result)
+
 
 def main():
     command = sys.argv[1]
@@ -39,7 +50,13 @@ def main():
         decoded_result = decode_bytes(result)  # Recursively decode bytes objects
         print(json.dumps(decoded_result))  # Pretty-print the result as JSON
     elif command == "info":
-        info(sys.argv[-1])
+        result, decoded_result = info(sys.argv[-1])
+        info_dict = bencodepy.encode(result[b'info'])
+        info_dict = hashlib.sha1(info_dict).hexdigest()
+        print(f"Tracker URL: {decoded_result['announce']} Length: {decoded_result['info']['length']} Info Hash: {info_dict} Piece Length: {decoded_result['info']['piece length']} Piece Hashed: {decoded_result['info']['pieces']}")
+    elif command == 'peers':
+        result, decoded_result = info(sys.argv[-1])
+        peers(result, decoded_result)
 
 
 if __name__ == "__main__":
